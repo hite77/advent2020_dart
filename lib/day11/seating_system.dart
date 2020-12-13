221c800855c2f10a9dfec84efeaccb0b4d979949
Jason Hite <hite77@gmail.com>
2020-12-12 20:22:28 -0500

    update to fix dat 11 for part 2

--- lib/day11/seating_system.dart
+++ lib/day11/seating_system.dart
@@ -3,13 +3,104 @@ import 'dart:core';
 import 'dart:io';
 
 List<String> map = [];
+List<String> nextState = [];
 
 addLine(line) {
   map.add(line);
 }
 
+int countOrZeroOutside(x, y, dirx, diry) {
+  var value = 0;
+  var i = x + dirx;
+  var j = y + diry;
+
+  // need to keep going in straight lines, if you encounter a L then 0
+  // keep moving dirx and diry
+  // or stepping out...
+
+  while (i >= 0 && i < map.length && j >= 0 && j < map[0].length) {
+    if (map[i][j] == '#') {
+      value = 1;
+      break;
+    }
+
+    if (map[i][j] == 'L') {
+      value = 0;
+      break;
+    }
+    i += dirx;
+    j += diry;
+  }
+  return value;
+}
+
+int countOfSeatsAround(i, j) {
+  return countOrZeroOutside(i, j, -1, -1) + // -1, -1
+      countOrZeroOutside(i, j, -1, 0) + // -1, 0
+      countOrZeroOutside(i, j, -1, 1) + // -1, +1
+      countOrZeroOutside(i, j, 0, -1) + // 0, -1
+      countOrZeroOutside(i, j, 0, 1) + //0, +1
+      countOrZeroOutside(i, j, 1, -1) + // 1, -1
+      countOrZeroOutside(i, j, 1, 0) + // 1, 0
+      countOrZeroOutside(i, j, 1, 1); // 1, 1
+}
+
+//If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+//If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+//Otherwise, the seat's state does not change.
+
+// morePlanets = new List<String>.from(planets)
+round() {
+  // updates using rules above....
+  nextState = []; //new List<String>.from(map);
+  for (var i = 0; i < map.length; i++) {
+    String newLine = '';
+    for (var j = 0; j < map[i].length; j++) {
+      var count = countOfSeatsAround(i, j);
+      if (map[i][j] == 'L' && count == 0) {
+        newLine += '#';
+      } else if (map[i][j] == '#' && count > 4) {
+        newLine += 'L';
+      } else {
+        newLine += map[i][j];
+      }
+    }
+    nextState.add(newLine);
+  }
+  map = new List<String>.from(nextState);
+}
+
+bool compare(map1, map2) {
+  var cont = false;
+  for (int i = 0; i < map1.length; i++) {
+    for (int j = 0; j < map1[i].length; j++) {
+      if (map1[i][j] != map2[i][j]) {
+        cont = true;
+        break;
+      }
+    }
+  }
+  return cont;
+}
+
 int answer() {
-  return 42;
+  // repeatedly call round until no changes, then count how many occupied..
+  int answer = 0;
+  List current = [];
+  do {
+    current = new List<String>.from(map);
+    round();
+  } while (compare(map, current));
+  print(map);
+  for (int i = 0; i < map.length; i++) {
+    for (int j = 0; j < map[i].length; j++) {
+      if (map[i][j] == '#') {
+        answer += 1;
+      }
+    }
+  }
+
+  return answer;
 }
 
 //todo: can I integration test the whole thing?
--- test/seating_system_test.dart
+++ test/seating_system_test.dart
@@ -3,7 +3,8 @@ import 'package:passport_app/day11/seating_system.dart';
 
 @Tags(const ['seating'])
 void main() {
-  test("addLine builds map", () {
+  setUp(() {
+    map.clear();
     addLine("L.LL.LL.LL");
     addLine("LLLLLLL.LL");
     addLine("L.L.L..L..");
@@ -14,6 +15,9 @@ void main() {
     addLine("LLLLLLLLLL");
     addLine("L.LLLLLL.L");
     addLine("L.LLLLL.LL");
+  });
+
+  test("addLine builds map", () {
     expect(map[0], 'L.LL.LL.LL');
     expect(map[1], 'LLLLLLL.LL');
     expect(map[2], 'L.L.L..L..');
@@ -24,5 +28,60 @@ void main() {
     expect(map[7], 'LLLLLLLLLL');
     expect(map[8], 'L.LLLLLL.L');
     expect(map[9], 'L.LLLLL.LL');
+    expect(map.length, 10);
+  });
+
+  test("next state is correct", () {
+    round();
+    // expect(map[0], '#.##.##.##');
+    // expect(map[1], '#######.##');
+    // expect(map[2], '#.#.#..#..');
+    // expect(map[3], '####.##.##');
+    // expect(map[4], '#.##.##.##');
+    // expect(map[5], '#.#####.##');
+    // expect(map[6], '..#.#.....');
+    // expect(map[7], '##########');
+    // expect(map[8], '#.######.#');
+    // expect(map[9], '#.#####.##');
+    expect(map[0], '#.##.##.##');
+    expect(map[1], '#######.##');
+    expect(map[2], '#.#.#..#..');
+    expect(map[3], '####.##.##');
+    expect(map[4], '#.##.##.##');
+    expect(map[5], '#.#####.##');
+    expect(map[6], '..#.#.....');
+    expect(map[7], '##########');
+    expect(map[8], '#.######.#');
+    expect(map[9], '#.#####.##');
+  });
+
+  test("next state is correct", () {
+    round();
+    round();
+    // expect(map[0], '#.LL.L#.##');
+    // expect(map[1], '#LLLLLL.L#');
+    // expect(map[2], 'L.L.L..L..');
+    // expect(map[3], '#LLL.LL.L#');
+    // expect(map[4], '#.LL.LL.LL');
+    // expect(map[5], '#.LLLL#.##');
+    // expect(map[6], '..L.L.....');
+    // expect(map[7], '#LLLLLLLL#');
+    // expect(map[8], '#.LLLLLL.L');
+    // expect(map[9], '#.#LLLL.##');
+    expect(map[0], '#.LL.LL.L#');
+    expect(map[1], '#LLLLLL.LL');
+    expect(map[2], 'L.L.L..L..');
+    expect(map[3], 'LLLL.LL.LL');
+    expect(map[4], 'L.LL.LL.LL');
+    expect(map[5], 'L.LLLLL.LL');
+    expect(map[6], '..L.L.....');
+    expect(map[7], 'LLLLLLLLL#');
+    expect(map[8], '#.LLLLLL.L');
+    expect(map[9], '#.LLLLL.L#');
+  });
+
+  test("correct answer", () {
+    // repeatedly call round until no changes....
+    expect(answer(), 26);
   });
 }
