2dac79bf040150301c23ef46cac8fd7cf52af359
Jason Hite <hite77@gmail.com>
2021-04-08 16:17:40 -0400

    day 23 working.  Brute forced part 2, took more than a day

--- lib/day23/crab_cups.dart
+++ lib/day23/crab_cups.dart
@@ -1,11 +1,210 @@
 import 'dart:core';
 
-String answer() {
-  return 'foo';
+List<int> items = [];
+List<int> holdThree = [];
+int current_cup;
+
+String answer(String s) {
+  return move(s, 100);
+}
+
+String answer2(String s) {
+  return move2(s, 10000000);
+}
+
+String move2(String s, int loops) {
+  items = [];
+  for (var i = 0; i < s.length; i++) {
+    if (i < s.length - 1) {
+      items.add(int.parse(s.substring(i, i + 1)));
+    } else {
+      items.add(int.parse(s.substring(i)));
+    }
+  }
+  int highest = items[0];
+  items.forEach((element) {
+    if (element > highest) {
+      highest = element;
+    }
+  });
+
+  while (highest < 1000000) {
+    highest += 1;
+    items.add(highest);
+  }
+
+  current_cup = items[0];
+
+  while (loops > 0) {
+    moveThreeCups(items, holdThree, current_cup);
+    var destination = updateDestination2(items, current_cup);
+    print('Hold: $holdThree Destination: $destination');
+    placeTheCups(items, destination, holdThree);
+    current_cup = clockwiseSelectCup(items, current_cup);
+    print('Current cup: $current_cup');
+    loops--;
+    print('$loops ${output2(items)} ${items.indexOf(1)}');
+  }
+  return output2(items);
+}
+
+String output2(List<int> items) {
+  String twoValues = '';
+  int values = 0;
+  int position_of_1 = items.indexOf(1);
+  if (position_of_1 < items.length - 1) {
+    for (var i = position_of_1 + 1; i < items.length; i++) {
+      twoValues += ' ${items[i]} ';
+      values += 1;
+      if (values == 2) {
+        return twoValues;
+      }
+    }
+  }
+  if (position_of_1 > 0) {
+    for (var i = 0; i < position_of_1; i++) {
+      twoValues += ' ${items[i]} ';
+      values += 1;
+      if (values == 2) {
+        return twoValues;
+      }
+    }
+  }
+}
+
+String output(List<int> items) {
+  String output = '';
+  int position_of_1 = items.indexOf(1);
+  if (position_of_1 < items.length - 1) {
+    for (var i = position_of_1 + 1; i < items.length; i++) {
+      output += '${items[i]}';
+    }
+  }
+  if (position_of_1 > 0) {
+    for (var i = 0; i < position_of_1; i++) {
+      output += '${items[i]}';
+    }
+  }
+  return output;
+}
+
+moveThreeCups(List<int> items, List<int> holdThree, current_cup) {
+  // The crab picks up the three cups that are immediately clockwise of
+  // the current cup. They are removed from the circle; cup spacing is
+  // adjusted as necessary to maintain the circle.
+  // first case current cup has three directly next to it...
+  int position_current_cup = items.indexOf(current_cup);
+
+  if (items.length - 1 - position_current_cup >= 3) {
+    holdThree.add(items[position_current_cup + 1]);
+    holdThree.add(items[position_current_cup + 2]);
+    holdThree.add(items[position_current_cup + 3]);
+    holdThree.forEach((element) {
+      items.remove(element);
+    });
+  } else {
+    // one, two possible....
+    for (var i = position_current_cup + 1; i < items.length; i++) {
+      holdThree.add(items[i]);
+    }
+    int leftOver = 3 - holdThree.length;
+    for (var i = 0; i < leftOver; i++) {
+      holdThree.add(items[i]);
+    }
+    holdThree.forEach((element) {
+      items.remove(element);
+    });
+  }
+  // otherwise it needs to wrap around...
+}
+
+updateDestination2(List<int> items, current_cup) {
+  // The crab selects a destination cup: the cup with a label equal to
+  // the current cup's label minus one. If this would select one of the
+  // cups that was just picked up, the crab will keep subtracting one
+  // until it finds a cup that wasn't just picked up. If at any point in
+  // this process the value goes below the lowest value on any cup's
+  // label, it wraps around to the highest value on any cup's label
+  // instead.
+  int try_cup = current_cup - 1;
+  while (!items.contains(try_cup)) {
+    if (try_cup < 1) {
+      try_cup = 1000000;
+    } else {
+      try_cup -= 1;
+    }
+  }
+
+  return try_cup;
+}
+
+updateDestination(List<int> items, current_cup) {
+  // The crab selects a destination cup: the cup with a label equal to
+  // the current cup's label minus one. If this would select one of the
+  // cups that was just picked up, the crab will keep subtracting one
+  // until it finds a cup that wasn't just picked up. If at any point in
+  // this process the value goes below the lowest value on any cup's
+  // label, it wraps around to the highest value on any cup's label
+  // instead.
+  int try_cup = current_cup - 1;
+  while (!items.contains(try_cup)) {
+    if (try_cup < 1) {
+      try_cup = 9;
+    } else {
+      try_cup -= 1;
+    }
+  }
+
+  return try_cup;
+}
+
+placeTheCups(List<int> items, int current_cup, List<int> holdThree) {
+  // The crab places the cups it just picked up so that they are
+  // immediately clockwise of the destination cup. They keep the same
+  // order as when they were picked up.
+  int position_current_cup = items.indexOf(current_cup);
+  items.insertAll(position_current_cup + 1, holdThree);
+  holdThree.clear();
+}
+
+clockwiseSelectCup(List<int> items, int current_cup) {
+  // The crab selects a new current cup: the cup which is immediately
+  // clockwise of the current cup.
+  int position_current_cup = items.indexOf(current_cup) + 1;
+
+  if (position_current_cup >= items.length) {
+    position_current_cup = 0;
+  }
+  return items[position_current_cup];
+}
+
+String move(String s, int loops) {
+  items = [];
+  for (var i = 0; i < s.length; i++) {
+    if (i < s.length - 1) {
+      items.add(int.parse(s.substring(i, i + 1)));
+    } else {
+      items.add(int.parse(s.substring(i)));
+    }
+  }
+  current_cup = items[0];
+
+  while (loops > 0) {
+    moveThreeCups(items, holdThree, current_cup);
+    var destination = updateDestination(items, current_cup);
+    placeTheCups(items, destination, holdThree);
+    current_cup = clockwiseSelectCup(items, current_cup);
+    loops--;
+  }
+
+  print(items);
+
+  return output(items);
 }
 
 void main() {
   String puzzleInput = '496138527';
 
-  print('Answer: ${answer()}');
+  print('Answer: ${answer(puzzleInput)}');
+  print('Answer2: ${answer2(puzzleInput)}');
 }
--- test/day23_crab_cups_test.dart
+++ test/day23_crab_cups_test.dart
@@ -1,9 +1,105 @@
 import 'dart:core';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:passport_app/day23/crab_cups.dart';
 
 void main() {
+  // labeled clockwise(your puzzle input)
+  // 32415 in circle 3,2,4,1,5 and back to 3
+  // before start current cup is first cup....
+  // for final there will be 100 moves....
+
+  // pick up three cups that are clockwise of the current cup
+  // select a destination cup, one with label equal to current cup label minus one.  It will keep subtracting one till it matches a cup in the circle if it wraps around to lowest it will wrap to highest.
+  // place the picked up 3 cups immediately clockwise
+  // Select a new current cup which is clockwise of the current cup
+
+  // finally take numbers from 1 clockwise.... and not including 1
+
   setUp(() {});
 
-  test('', () {});
+  test('output a string', () {
+    expect(output([3, 8, 9, 1, 2, 5, 4, 6, 7]), '25467389');
+    expect(output([3, 2, 8, 9, 1, 5, 4, 6, 7]), '54673289');
+    expect(output([1, 2, 3, 4, 5, 6, 7, 8]), '2345678');
+    expect(output([2, 3, 5, 6, 7, 1]), '23567');
+    expect(output([5, 6, 1, 8]), '856');
+  });
+
+  test('moveThreeCupsHappyPath', () {
+    List<int> items = [3, 8, 9, 1, 2, 5, 4, 6, 7];
+    List<int> holdThree = [];
+    int current_cup = 3;
+    moveThreeCups(items, holdThree, current_cup);
+    expect(items, [3, 2, 5, 4, 6, 7]);
+    expect(holdThree, [8, 9, 1]);
+  });
+
+  test('moveThreeCupsLoopAround', () {
+    List<int> items = [7, 2, 5, 8, 4, 1, 9, 3, 6];
+    List<int> holdThree = [];
+    int current_cup = 9;
+    moveThreeCups(items, holdThree, current_cup);
+    expect(items, [2, 5, 8, 4, 1, 9]);
+    expect(holdThree, [3, 6, 7]);
+    items = [7, 2, 5, 8, 4, 1, 9, 3, 6];
+    holdThree = [];
+    current_cup = 3;
+    moveThreeCups(items, holdThree, current_cup);
+    expect(items, [5, 8, 4, 1, 9, 3]);
+    expect(holdThree, [6, 7, 2]);
+    items = [7, 2, 5, 8, 4, 1, 9, 3, 6];
+    holdThree = [];
+    current_cup = 6;
+    moveThreeCups(items, holdThree, current_cup);
+    expect(items, [8, 4, 1, 9, 3, 6]);
+    expect(holdThree, [7, 2, 5]);
+  });
+
+  test('updateDestination', () {
+    expect(updateDestination([1, 2, 3, 4, 5], 3), 2);
+    expect(updateDestination([1, 2, 3, 4, 5], 2), 1);
+    expect(updateDestination([1, 2, 3, 4, 5], 1), 5);
+    expect(updateDestination([1, 2, 3, 4, 5], 9), 5);
+    expect(updateDestination([1, 5, 7, 8], 5), 1);
+  });
+
+  test('placeTheCups', () {
+    List<int> items = [1, 2, 3, 4, 5];
+    List<int> holdThree = [6, 7, 8];
+    int current_cup = 5;
+    placeTheCups(items, current_cup, holdThree);
+    expect(holdThree, []);
+    expect(items, [1, 2, 3, 4, 5, 6, 7, 8]);
+    holdThree = [9, 11, 10];
+    current_cup = 1;
+    placeTheCups(items, current_cup, holdThree);
+    expect(holdThree, []);
+    expect(items, [1, 9, 11, 10, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  test('clockWiseSelectCup', () {
+    expect(clockwiseSelectCup([9, 4, 3, 6, 7], 9), 4);
+    expect(clockwiseSelectCup([9, 4, 3, 6, 7], 4), 3);
+    expect(clockwiseSelectCup([9, 4, 3, 6, 7], 3), 6);
+    expect(clockwiseSelectCup([9, 4, 3, 6, 7], 6), 7);
+    expect(clockwiseSelectCup([9, 4, 3, 6, 7], 7), 9);
+  });
+
+  test('test move 1', () {
+    expect(move('389125467', 1), '54673289');
+  });
+
+  test('test move 2', () {
+    expect(move('389125467', 2), '32546789');
+  });
+
+  test('test move 10', () {
+    expect(move('389125467', 10), '92658374');
+  });
+
+  test('answer to part 1', () {
+    String a = answer('496138527');
+    expect(a, 'Answer?');
+  });
 }
